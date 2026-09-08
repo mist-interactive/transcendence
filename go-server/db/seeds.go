a05d89c272fc428d029aded09ef0ff3fd867ecb1
nraatika <nraatika@student.hive.fi>
2026-09-08 10:46:20 +0300

    test(matches): add unit tests for match patch and score seeding

--- go-server/db/seeds.go
+++ go-server/db/seeds.go
@@ -37,15 +37,32 @@ func SeedDevDatabase(ctx context.Context, db *bun.DB) error {
 		return err
 	}
 
-	// generate 50 matches with varied pairings
-	win := models.ResultPlayer1Win
+	// generate 50 matches with varied pairings and scores
 	matches := make([]models.MatchRecord, 50)
 	for i := range 50 {
+		var win models.MatchResult
+		var p1Score, p2Score int
+		switch i % 3 {
+		case 0:
+			win = models.ResultPlayer1Win
+			p1Score = 5 + (i % 3)
+			p2Score = 2 + (i % 2)
+		case 1:
+			win = models.ResultPlayer2Win
+			p1Score = 1 + (i % 3)
+			p2Score = 6 + (i % 2)
+		default:
+			win = models.ResultDraw
+			p1Score = 4
+			p2Score = 4
+		}
 		matches[i] = models.MatchRecord{
-			Player1: users[i%len(users)].ID,
-			Player2: users[(i+3)%len(users)].ID,
-			Status:  models.StatusFinished,
-			Result:  &win,
+			Player1:      users[i%len(users)].ID,
+			Player2:      users[(i+3)%len(users)].ID,
+			Player1Score: &p1Score,
+			Player2Score: &p2Score,
+			Status:       models.StatusFinished,
+			Result:       &win,
 		}
 	}
 	_, err = db.NewInsert().Model(&matches).Exec(ctx)
